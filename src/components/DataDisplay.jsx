2c3753aef34edd58a60060ec3b5572338068ba48
victorzottmann <zottmann.victor@gmail.com>
2021-05-28 12:24:44 +1000

    add chart js

--- src/components/DataDisplay.jsx
+++ src/components/DataDisplay.jsx
@@ -1,18 +1,53 @@
 import React from 'react'
+import { Line, Chart } from 'react-chartjs-2'
 
 const DataDisplay = ({ currency, data }) => {
-  console.log(Object.entries(data))
+  Chart.defaults.font.color = '#000'
+  Chart.defaults.font.size = 16
+
+  function formatChartData() {
+    return {
+      labels: Object.keys(data),
+      datasets: [
+        {
+          label: 'Bitcoin',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: Object.values(data),
+        },
+      ],
+    }
+  }
+
   return (
     <div>
       <h2>Bitcon Index in {currency}</h2>
-      {Object.entries(data).map((element) => {
+      <div style={{ margin: '1rem' }}>
+        <Line data={formatChartData()} />
+      </div>
+      {/* {Object.entries(data).map((element) => {
         const [date, value] = element
         return (
           <div key={date}>
             Date: {date} Value: {value}
           </div>
         )
-      })}
+      })} */}
     </div>
   )
 }
